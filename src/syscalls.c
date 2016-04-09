b11607baf2ab06e90e0083532a5a797746ada318
Louis 'Kureuil' Person <louis.person@epitech.eu>
2016-04-09 23:37:08 +0200

    Done syscalls for ls

--- src/syscalls.c
+++ src/syscalls.c
@@ -5,7 +5,7 @@
 ** Login   <vagrant@epitech.net>
 **
 ** Started on  Sat Apr  9 19:30:52 2016
-** Last update Sat Apr  9 23:10:48 2016 Arch Kureuil
+** Last update Sat Apr  9 23:36:45 2016 Arch Kureuil
 */
 
 #include <stdlib.h>
@@ -65,6 +65,105 @@ struct s_syscall	g_syscalls[] = {
 	},
       },
     },
+  },
+  {
+    .id = 2,
+    .name = "open",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_STRING
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      },
+    }
+  },
+  {
+    .id = 3,
+    .name = "close",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 1,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      }
+    }
+  },
+  {
+    .id = 4,
+    .name = "stat",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_STRING
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_POINTER
+	}
+      }
+    },
+  },
+  {
+    .id = 5,
+    .name = "fstat",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER,
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_POINTER
+	}
+      },
+    }
+  },
+  {
+    .id = 6,
+    .name = "lstat",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_STRING
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_POINTER
+	}
+      }
+    }
   },
   {
     .id = 9,
@@ -175,6 +274,27 @@ struct s_syscall	g_syscalls[] = {
     }
   },
   {
+    .id = 16,
+    .name = "ioctl",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      }
+    }
+  },
+  {
     .id = 21,
     .name = "access",
     .noreturn = false,
@@ -209,6 +329,117 @@ struct s_syscall	g_syscalls[] = {
 	},
       }
     },
+  },
+  {
+    .id = 78,
+    .name = "getdents",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 3,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER,
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_POINTER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_SIZE_T
+	}
+      }
+    },
+  },
+  {
+    .id = 137,
+    .name = "statfs",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_STRING
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_POINTER
+	}
+      }
+    }
+  },
+  {
+    .id = 158,
+    .name = "arch_prctl",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_ULONG
+	}
+      }
+    }
+  },
+  {
+    .id = 231,
+    .name = "exit_group",
+    .noreturn = true,
+    .retval = T_DEFAULT,
+    .argc = 1,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      }
+    },
+  },
+  {
+    .id = 257,
+    .name = "openat",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 3,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_STRING
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      }
+    }
   },
   {
     .id = (unsigned long long) -1,
