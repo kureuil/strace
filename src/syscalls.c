761693473a1b8f2f22d55eed6069b372d789a676
Louis 'Kureuil' Person <louis.person@epitech.eu>
2016-04-09 23:10:59 +0200

    Add some syscalls

--- src/syscalls.c
+++ src/syscalls.c
@@ -5,7 +5,7 @@
 ** Login   <vagrant@epitech.net>
 **
 ** Started on  Sat Apr  9 19:30:52 2016
-** Last update Sat Apr  9 19:41:12 2016 
+** Last update Sat Apr  9 23:10:48 2016 Arch Kureuil
 */
 
 #include <stdlib.h>
@@ -65,6 +65,135 @@ struct s_syscall	g_syscalls[] = {
 	},
       },
     },
+  },
+  {
+    .id = 9,
+    .name = "mmap",
+    .noreturn = false,
+    .retval = T_POINTER,
+    .argc = 6,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_POINTER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_SIZE_T
+	},
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_LONG
+	}
+      },
+    }
+  },
+  {
+    .id = 10,
+    .name = "mprotect",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 3,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_POINTER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_SIZE_T
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      }
+    }
+  },
+  {
+    .id = 11,
+    .name = "munmap",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_POINTER
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_SIZE_T
+	}
+      }
+    }
+  },
+  {
+    .id = 12,
+    .name = "brk",
+    .noreturn = false,
+    .retval = T_POINTER,
+    .argc = 1,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_ULONG,
+	},
+      },
+    }
+  },
+  {
+    .id = 21,
+    .name = "access",
+    .noreturn = false,
+    .retval = T_INTEGER,
+    .argc = 2,
+    .args = {
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_STRING
+	}
+      },
+      {
+	.custom = false,
+	.printer = {
+	  .type = T_INTEGER
+	}
+      },
+    }
   },
   {
     .id = 60,
